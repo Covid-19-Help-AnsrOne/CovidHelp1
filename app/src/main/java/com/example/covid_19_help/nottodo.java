2f615a4cece240965a8a79ab65c7e1b1ab02d1c6
naveen12n <naveenshetty.127@gmail.com>
2021-06-22 12:33:54 +0530

    home isolation

---
+++ app/src/main/java/com/example/covid_19_help/nottodo.java
@@ -0,0 +1,14 @@
+package com.example.covid_19_help;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nottodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nottodo);
+    }
+}
