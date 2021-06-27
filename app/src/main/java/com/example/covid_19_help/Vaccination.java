8fc5bba99229c37866be26a7d932def11d94e8f4
AyushGupta0202 <ayushg430@gmail.com>
2021-06-27 17:14:31 +0530

    Added progress bar in Vaccination Activity

--- app/src/main/java/com/example/covid_19_help/Vaccination.java
+++ app/src/main/java/com/example/covid_19_help/Vaccination.java
@@ -7,22 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Vaccination extends AppCompatActivity {
 
     WebView web;
-
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccination);
         web = findViewById(R.id.webView);
+
+        // Circular progress bar before the url loads in the webView.
+        progress = findViewById(R.id.progress);
         WebSettings webSettings = web.getSettings();
         webSettings.setJavaScriptEnabled(true);
         web.setWebViewClient(new Callback());
@@ -46,6 +51,18 @@ public class Vaccination extends AppCompatActivity {
         @Override
         public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
             return false;
+        }
+
+        // This method is called after the url finishes loading.
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+            // Make the progress bar invisible after the loading completes.
+            progress.setVisibility(View.GONE);
+
+            // Make the web view visible after it loads the contents.
+            web.setVisibility(View.VISIBLE);
         }
     }
 
