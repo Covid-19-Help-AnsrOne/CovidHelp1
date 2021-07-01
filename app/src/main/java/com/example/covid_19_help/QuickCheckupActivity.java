ece753ff1e24e6232ed02e5eedb308ef6939e36b
AyushGupta0202 <ayushg430@gmail.com>
2021-07-01 12:19:20 +0530

    Quick Checkup fixed

--- app/src/main/java/com/example/covid_19_help/QuickCheckupActivity.java
+++ app/src/main/java/com/example/covid_19_help/QuickCheckupActivity.java
@@ -1,7 +1,5 @@
 package com.example.covid_19_help;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,19 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-//Quick checkup activity with a chat bot.
-public class QuickCheckupActivity extends AppCompatActivity implements View.OnClickListener{
+import androidx.appcompat.app.AppCompatActivity;
 
-   private TextView tvVaccine, tvHowMany, tvAgePrompt, btnNo, btnYes, tvOne, tvTwo,
-           btnNoDoseZero, btnYesDoseZero, btnPos, btnNeg;
-   private EditText etAge;
-   private Button btnConfirm;
-   private TextView tvDoseResult, tvCovidPositiveOrNot, tvCovidPositiveMessage;
-   View btnContain, doses, btnDoseContainZero, btnCovPosOrNot;
-   boolean flag = false;
+//Quick checkup activity with a chat bot.
+public class QuickCheckupActivity extends AppCompatActivity implements View.OnClickListener {
+
+   TextView tvVaccine, tvHowMany, tvAgePrompt, btnNo, btnYes, tvOne, tvTwo,
+           btnYesTested, btnNoTested, btnPos, btnNeg,
+           btnTravelledYes, btnTravelledNo, btnSymptomsYes, btnSymptomsNo, tvRecentlyTested,
+           tvTravelledRecently, tvHaveSymptoms, tvLastFinalMessage;
+   EditText etAge;
+   Button btnConfirm;
+   TextView tvDoseResult, tvCovidPositiveOrNot, tvCovidPositiveMessage;
+   View btnContainTakenVaccine, btnContainNumberOfDoses,
+           btnContainTestedForCovid, btnContainCovPosOrNot, btnContainTravelledOrNot,
+           btnContainHaveSymptomsOrNot;
+   boolean haveTravelled;
 
    //Find all the views.
    private void findViews() {
+      btnContainHaveSymptomsOrNot = findViewById(R.id.btnContainHaveSymptomsOrNot);
+      btnContainTravelledOrNot = findViewById(R.id.btnContainTravelledOrNot);
+      tvTravelledRecently = findViewById(R.id.tvTravelledRecently);
+      tvHaveSymptoms = findViewById(R.id.tvHaveSymptoms);
+      tvLastFinalMessage = findViewById(R.id.tvLastFinalMessage);
+      btnTravelledNo = findViewById(R.id.btnTravelledNo);
+      btnTravelledYes = findViewById(R.id.btnTravelledYes);
+      btnSymptomsNo = findViewById(R.id.btnSymptomsNo);
+      btnSymptomsYes = findViewById(R.id.btnSymptomsYes);
       etAge = findViewById(R.id.etAge);
       tvHowMany = findViewById(R.id.tvHowMany);
       tvAgePrompt = findViewById(R.id.tvAgePrompt);
@@ -30,19 +43,21 @@ public class QuickCheckupActivity extends AppCompatActivity implements View.OnCl
       btnConfirm = findViewById(R.id.btnConfirmAge);
       btnNo = findViewById(R.id.btnNo);
       btnYes = findViewById(R.id.btnYes);
-      btnNoDoseZero = findViewById(R.id.btnNoDoseZero);
-      btnYesDoseZero = findViewById(R.id.btnYesDoseZero);
+      btnYesTested = findViewById(R.id.btnYesTested);
+      btnNoTested = findViewById(R.id.btnNoTested);
       tvOne = findViewById(R.id.tvOne);
       tvTwo = findViewById(R.id.tvTwo);
       tvDoseResult = findViewById(R.id.tvDoseResult);
-      btnContain = findViewById(R.id.btnContain);
-      doses = findViewById(R.id.doses);
-      btnDoseContainZero = findViewById(R.id.btnContainDoseZero);
+      btnContainTakenVaccine = findViewById(R.id.btnContainTakenVaccine);
+      btnContainNumberOfDoses = findViewById(R.id.containNumberOfDoses);
+      btnContainTestedForCovid = findViewById(R.id.btnContainTestedForCovid);
       btnPos = findViewById(R.id.btnPos);
       btnNeg = findViewById(R.id.btnNeg);
       tvCovidPositiveOrNot = findViewById(R.id.tvCovidPositiveOrNot);
       tvCovidPositiveMessage = findViewById(R.id.tvCovidPositiveMessage);
-      btnCovPosOrNot = findViewById(R.id.btnCovPosOrNot);
+      btnContainCovPosOrNot = findViewById(R.id.btnContainCovPosOrNot);
+      tvRecentlyTested = findViewById(R.id.tvRecentlyTested);
+
    }
 
    @Override
@@ -66,12 +81,17 @@ public class QuickCheckupActivity extends AppCompatActivity implements View.OnCl
       tvTwo.setOnClickListener(this);
       tvOne.setOnClickListener(this);
 
-      btnNoDoseZero.setOnClickListener(this);
-
-      btnYesDoseZero.setOnClickListener(this);
+      btnYesTested.setOnClickListener(this);
+      btnNoTested.setOnClickListener(this);
 
       btnPos.setOnClickListener(this);
       btnNeg.setOnClickListener(this);
+
+      btnTravelledNo.setOnClickListener(this);
+      btnTravelledYes.setOnClickListener(this);
+
+      btnSymptomsNo.setOnClickListener(this);
+      btnSymptomsYes.setOnClickListener(this);
    }
 
    //Method to ask the age of the user.
@@ -84,30 +104,30 @@ public class QuickCheckupActivity extends AppCompatActivity implements View.OnCl
    }
 
 
-
    @SuppressLint("NonConstantResourceId")
    @Override
    public void onClick(View view) {
       switch (view.getId()) {
          case R.id.btnConfirmAge:
-             if (etAge.getText().toString().isEmpty()) etAge.setError("Age required!");
-         else {
-            etAge.setEnabled(false);
-            btnConfirm.setVisibility(View.GONE);
-            tvVaccine.setVisibility(View.VISIBLE);
-            btnContain.setVisibility(View.VISIBLE);
-         }
-         break;
+            if (etAge.getText().toString().isEmpty()) etAge.setError("Age required!");
+            else {
+               etAge.setEnabled(false);
+               btnConfirm.setVisibility(View.GONE);
+               tvVaccine.setVisibility(View.VISIBLE);
+               btnContainTakenVaccine.setVisibility(View.VISIBLE);
+            }
+            break;
          case R.id.btnNo:
             btnYes.setVisibility(View.GONE);
-            tvHowMany.setVisibility(View.VISIBLE);
-            tvHowMany.setText(getResources().getString(R.string.dose_zero));
-            btnDoseContainZero.setVisibility(View.VISIBLE);
+            tvRecentlyTested.setVisibility(View.VISIBLE);
+            tvRecentlyTested.setText(getResources().getString(R.string.recently_tested));
+            btnContainTestedForCovid.setVisibility(View.VISIBLE);
             break;
+
          case R.id.btnYes:
             btnNo.setVisibility(View.GONE);
             tvHowMany.setVisibility(View.VISIBLE);
-            doses.setVisibility(View.VISIBLE);
+            btnContainNumberOfDoses.setVisibility(View.VISIBLE);
             break;
          case R.id.tvOne:
             tvTwo.setVisibility(View.GONE);
@@ -119,42 +139,67 @@ public class QuickCheckupActivity extends AppCompatActivity implements View.OnCl
             tvDoseResult.setText(getResources().getString(R.string.dose_two));
             tvDoseResult.setVisibility(View.VISIBLE);
             break;
-         case R.id.btnYesDoseZero:
-            flag = true;
-            btnNoDoseZero.setVisibility(View.GONE);
+
+         case R.id.btnYesTested:
+            btnNoTested.setVisibility(View.GONE);
             tvCovidPositiveOrNot.setVisibility(View.VISIBLE);
-            btnCovPosOrNot.setVisibility(View.VISIBLE);
+            btnContainCovPosOrNot.setVisibility(View.VISIBLE);
             tvCovidPositiveOrNot.setText(getResources().getString(R.string.positive_or_not));
             break;
-         case R.id.btnNoDoseZero:
-            flag = false;
-            btnYesDoseZero.setVisibility(View.GONE);
-            tvCovidPositiveMessage.setVisibility(View.VISIBLE);
-            tvCovidPositiveMessage.setText(getResources().getString(R.string.symptoms));
-            btnCovPosOrNot.setVisibility(View.VISIBLE);
-            //TODO:
+         case R.id.btnNoTested:
+            btnYesTested.setVisibility(View.GONE);
+            tvTravelledRecently.setVisibility(View.VISIBLE);
+            btnContainTravelledOrNot.setVisibility(View.VISIBLE);
+            tvTravelledRecently.setText(getResources().getString(R.string.travelled));
             break;
 
          case R.id.btnNeg:
-            if (flag) {
-               tvCovidPositiveMessage.setVisibility(View.VISIBLE);
                btnPos.setVisibility(View.GONE);
-               tvCovidPositiveMessage.setText(getResources().getString(R.string.covid_negative));
-            } else {
                tvCovidPositiveMessage.setVisibility(View.VISIBLE);
-               btnPos.setVisibility(View.GONE);
                tvCovidPositiveMessage.setText(getResources().getString(R.string.covid_negative));
-            }
             break;
          case R.id.btnPos:
-            if (flag) {
                tvCovidPositiveMessage.setVisibility(View.VISIBLE);
                btnNeg.setVisibility(View.GONE);
-               tvCovidPositiveMessage.setText(getResources().getString(R.string.you_have_symptoms));
+               tvCovidPositiveMessage.setText(getResources().getString(R.string.covid_positive));
+            break;
+
+         case R.id.btnTravelledYes:
+            haveTravelled = true;
+            btnTravelledNo.setVisibility(View.GONE);
+            tvHaveSymptoms.setVisibility(View.VISIBLE);
+            btnContainHaveSymptomsOrNot.setVisibility(View.VISIBLE);
+            tvHaveSymptoms.setText(getResources().getString(R.string.symptoms));
+            break;
+         case R.id.btnTravelledNo:
+            haveTravelled = false;
+            btnTravelledYes.setVisibility(View.GONE);
+            tvHaveSymptoms.setVisibility(View.VISIBLE);
+            btnContainHaveSymptomsOrNot.setVisibility(View.VISIBLE);
+            tvHaveSymptoms.setText(getResources().getString(R.string.symptoms));
+            break;
+         case R.id.btnSymptomsYes:
+            btnSymptomsNo.setVisibility(View.GONE);
+            tvLastFinalMessage.setVisibility(View.VISIBLE);
+
+            if (haveTravelled) {
+               tvLastFinalMessage.setText(getResources().getString(R.string.travel_and_symptoms));
             } else {
-               tvCovidPositiveMessage.setVisibility(View.VISIBLE);
-               btnNeg.setVisibility(View.GONE);
-               tvCovidPositiveMessage.setText(getResources().getString(R.string.isolation));
+
+               tvLastFinalMessage.setText(getResources().getString(R.string.no_travel_but_yes_symptom));
+            }
+
+            break;
+         case R.id.btnSymptomsNo:
+            btnSymptomsYes.setVisibility(View.GONE);
+            tvLastFinalMessage.setVisibility(View.VISIBLE);
+
+            if (haveTravelled) {
+               tvLastFinalMessage.setText(getResources().getString(R.string.travel_but_no_symptom));
+
+            } else {
+               tvLastFinalMessage.setText(getResources().getString(R.string.no_travel_no_symptom));
+
             }
             break;
       }
